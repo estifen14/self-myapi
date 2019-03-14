4dea9cc8bf7a79e54ff26cc07cb06c4c3c13249c
estifen <eabrea@stratpoint.com>
2019-03-14 15:16:51 +0800

    add spring boot

---
+++ src/main/java/self/myapi/selfmyapi/SelfMyapiApplication.java
@@ -0,0 +1,13 @@
+package self.myapi.selfmyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfMyapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfMyapiApplication.class, args);
+	}
+
+}
